cf841871b08a853823dfa16e8f616e4da8875548
Usama Riaz <usamariaz113@gmail.com>
2025-12-19 01:51:33 +0500

    Manage the threshold with product  threshold field

---
+++ Domain/DTO/LowStockProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO
+{
+    public class LowStockProductDto
+    {
+        public string ProductName { get; set; }
+        public int CurrentStock { get; set; }
+        public int Threshold { get; set; }
+        public string Status { get; set; }
+    }
+}
